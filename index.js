738f3f54e73329ccedb85f4d536324f5d45e2b87
jnoyon <jihadur51@gmail.com>
2025-01-08 20:05:43 +0600

    Added FindOne Operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
@@ -37,6 +37,13 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     });
+
+    app.get('/rooms/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    })
 
 
   } finally {
