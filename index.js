5ca6b41cd64b5aeb43114eda3e0834f8fcc2a80c
jnoyon <jihadur51@gmail.com>
2025-01-09 14:59:09 +0600

    Room Booking POST and GET Api created

--- index.js
+++ index.js
@@ -24,13 +24,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
     const roomsCollection = client.db('StockRoom').collection('roomCollection');
+    const bookedCollection = client.db('StockRoom').collection('bookedCollection');
 
     app.get('/rooms', async(req, res)=> {
         const cursor = roomsCollection.find();
@@ -44,6 +45,26 @@ async function run() {
       const result = await roomsCollection.findOne(query);
       res.send(result);
     })
+
+    // Room Booking API
+    app.post('/room-bookings', async(req, res) => {
+      const bookedRoom = req.body;
+      const result = await bookedCollection.insertOne(bookedRoom);
+      res.send(result);
+    })
+
+    app.get('/room-bookings', async(req, res)=> {
+      const cursor = bookedCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+
+  app.get('/room-bookings/:id', async(req, res)=> {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await bookedCollection.findOne(query);
+    res.send(result);
+  })
 
 
   } finally {
